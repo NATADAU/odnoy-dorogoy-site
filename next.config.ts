9569a44cb9a0474598c60fec9a6b5ce2f701ab02
NATADAU <nata.daut@gmail.com>
2026-08-20 13:14:24 +0300

    Подготовить проект к резервной публикации

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from "next";
 
-const isProduction = process.env.NODE_ENV === "production";
+const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
 
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
-  basePath: isProduction ? "/odnoy-dorogoy-site" : "",
-  assetPrefix: isProduction ? "/odnoy-dorogoy-site/" : "",
+  basePath: isGitHubPages ? "/odnoy-dorogoy-site" : "",
+  assetPrefix: isGitHubPages ? "/odnoy-dorogoy-site/" : "",
   images: { unoptimized: true },
 };
 
