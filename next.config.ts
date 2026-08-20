dc14809d5ac20fe40c7d8259113b32148d626e0a
NATADAU <nata.daut@gmail.com>
2026-08-20 13:22:23 +0300

    Добавить резервную копию для согласования

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next";
 
 const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
+const isPortablePreview = process.env.PORTABLE_BUILD === "true";
 
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   basePath: isGitHubPages ? "/odnoy-dorogoy-site" : "",
-  assetPrefix: isGitHubPages ? "/odnoy-dorogoy-site/" : "",
+  assetPrefix: isGitHubPages ? "/odnoy-dorogoy-site/" : isPortablePreview ? "." : "",
   images: { unoptimized: true },
 };
 
---
+++ preview/_next/static/chunks/next/dist/client/components/builtin/global-error-4e035396889ab49e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[337],{3075:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,1570,23))}},e=>{e.O(0,[295,156,358],()=>e(e.s=3075)),_N_E=e.O()}]);
